2a79697bf20ab9747ca8d1e882513a0c0935fd96
Nemesis24 <guillaumejonquiere@gmail.com>
2025-01-10 08:46:22 +0100

    Future deprecated

--- custom_components/area_media_players/switch.py
+++ custom_components/area_media_players/switch.py
@@ -141,13 +141,13 @@ class RoomMediaPlayersSwitch(SwitchEntity):
         await super().async_added_to_hass()
         
         @callback
-        def async_state_changed(*_):
+        def async_state_changed(event):
             """Handle child updates."""
             self.async_schedule_update_ha_state(True)
 
         for player in self._players:
             self.async_on_remove(
-                self.hass.helpers.event.async_track_state_change(
+                self.hass.helpers.event.async_track_state_change_event(
                     player, async_state_changed
                 )
             )
